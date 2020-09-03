e103782a9f064c01888e389ff24df3b6db507f09
gabefraser <gabe@gabefraser.com>
2020-09-03 22:41:20 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tailwind CSS with LESS & Grunt
+
+This is a super simple starter project if you're looking to use Grunt with Tailwind CSS & LESS.
+
+## Get started
+
+To install all the required packages:
+`npm install`
+
+To compile the code (one-time):
+`grunt`
+
+To compile the code (on the fly):
+`grunt watch`
+
+## How to use
+
+Create your `.less` files inside `stylesheets/less/` and `@import "<path/to/file>";` to the `styles.less`.
+
+Please refer to the [Tailwind CSS Documentation](https://tailwindcss.com/docs/) for anything Tailwind CSS related.
+
+## Credits
+
+[Tailwind CSS](https://tailwindcss.com) for creating a mobile-first/utility framework. 
+[TailwindToolbox.com - Minimal Blog](https://www.tailwindtoolbox.com/templates/minimal-blog) for the default `index.html` demonstrating the starter project.
---
+++ gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function (grunt){
+    require('jit-grunt')(grunt);
+    
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2,
+                    sourcemap: true
+                },
+                files: {
+                    "stylesheets/css/styles.css": "stylesheets/less/styles.less"
+                }
+            }
+        },
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('tailwindcss'),
+                    require('autoprefixer')({ overrideBrowserslist: 'last 3 versions' }),
+                ]
+            },
+            dist: {
+                src: 'stylesheets/css/styles.css'
+            }
+        },
+        watch: {
+            styles: {
+                files: ['stylesheets/less/**/*.less', 'tailwind.config.js'],
+                tasks: ['less', 'postcss'],
+                options: {
+                    nospawn: true
+                }
+            }
+        },
+    });
+
+    // Load Tasks
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-cssnano');
+
+    // Registering tasks
+    grunt.registerTask('default', ['less', 'postcss']);
+}
